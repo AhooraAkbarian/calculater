d1e726330616e2a1c494388f2b02ac34941c0fdd
Ahoora <mr.ahoora.akbarian@gmail.com>
2025-09-09 20:05:11 +0330

    new update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const display = document.querySelector('span');
 const numberButtons = document.querySelectorAll('.num-btn');
 const clear =document.getElementById('num-3');
+const plus = document.getElementById('plus');
+const multiply = document.getElementById('multiply');
+const minus = document.getElementById('minus');
+const devied = document.getElementById('devied');
+const equal = document.getElementById('equal');
+
+let lastOperition = '';
+let memory = 0;
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function () {
@@ -13,3 +21,35 @@ numberButtons.forEach(button => {
 clear.addEventListener('click' , ()=>{
     display.textContent=0;
 })
+
+plus.addEventListener('click' , ()=>{
+    lastOperition = 'plus';
+    memory = display.textContent;
+    display.textContent = Number(display.textContent);
+    display.textContent = 0;
+});
+
+equal.addEventListener('click' , ()=>{
+    lastOperition = 'equal';
+    memory = display.textContent;
+    display.textContent = Number(display.textContent);
+    display.textContent = 0;
+});
+multiply.addEventListener('click' , ()=>{
+    lastOperition = 'multiply';
+    memory = display.textContent;
+    display.textContent = Number(display.textContent);
+    display.textContent = 0;
+});
+minus.addEventListener('click' , ()=>{
+    lastOperition = 'minus';
+    memory = display.textContent;
+    display.textContent = Number(display.textContent);
+    display.textContent = 0;
+});
+devied.addEventListener('click' , ()=>{
+    lastOperition = 'devied';
+    memory = display.textContent;
+    display.textContent = Number(display.textContent);
+    display.textContent = 0;
+});
